1fc563700f377d6e9b798d4dd78c07f317cdcf3b
zhangyanfang123 <635368604@qq.com>
2018-04-17 23:54:26 +0800

    pages

--- WEB3.0控件开发包V1.0.9_CN_带配置库/demo/cn/demo.js
+++ WEB3.0控件开发包V1.0.9_CN_带配置库/demo/cn/demo.js
@@ -14,7 +14,7 @@ $(function () {
     }
 	
 	// 初始化插件参数及插入插件
-	WebVideoCtrl.I_InitPlugin(500, 300, {
+	WebVideoCtrl.I_InitPlugin( document.body.clientWidth*0.9,720, {
         bWndFull: true,//是否支持单窗口双击全屏，默认支持 true:支持 false:不支持
         iWndowType: 2,
 		cbSelWnd: function (xmlDoc) {
@@ -227,7 +227,8 @@ function clickLogin() {
 
 			$("#ip").prepend("<option value='" + szIP + "'>" + szIP + "</option>");
 			setTimeout(function () {
-				$("#ip").val(szIP);
+				$("#ip").val(szIP
+);
 				getChannelInfo();
 			}, 10);
 		},
